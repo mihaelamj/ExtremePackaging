b01689960bc4b518a1809f172314f7fa5c09dfd2
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2025-03-31 17:41:44 +0200

    add app target

---
+++ Apps/SwiftUI/SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  SwiftUI
+//
+//  Created by Mihaela Mihaljevic Jakic on 31.03.2025..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
