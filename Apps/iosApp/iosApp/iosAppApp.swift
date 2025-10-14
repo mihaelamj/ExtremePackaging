d8b69f1d17b181689dc50898e0858f1bf28054c7
Mihaela Mihaljevic <mihaelamj@me.com>
2025-10-14 18:18:17 +0200

    added app targets

---
+++ Apps/iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Mihaela Mihaljevic on 14.10.2025..
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/macApp/macApp/macAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  macAppApp.swift
+//  macApp
+//
+//  Created by Mihaela Mihaljevic on 14.10.2025..
+//
+
+import SwiftUI
+
+@main
+struct macAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
